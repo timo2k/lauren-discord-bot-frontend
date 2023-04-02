6527953c85ab180e91a4272623427c354e3f7673
Timo Ischen <info@timo-ischen.de>
2023-04-02 14:55:55 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,59 @@
+<div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+	<div class="sm:mx-auto sm:w-full sm:max-w-md">
+		<h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+			Logge dich hier ein
+		</h2>
+		<p class="mt-2 text-center text-sm text-gray-600">Verpiss dich wenn du keinen Account hast</p>
+	</div>
+
+	<div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+		<div class="bg-white px-4 py-8 shadow sm:rounded-lg sm:px:10">
+			<form class="space-y-6">
+				<!-- Form Field Start -->
+				<div>
+					<label for="" class="block text-sm font-medium leading-6 text-gray-900"
+						>E-Mail Adresse</label
+					>
+					<div class="mt-2">
+						<input
+							placeholder="fuckyou@deine-mutter.gay"
+							type="email"
+							required
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+				<!-- Form Field End -->
+
+				<!-- Form Field Start -->
+				<div>
+					<label for="" class="block text-sm font-medium leading-6 text-gray-900">Passwort</label>
+					<div class="mt-2">
+						<input
+							required
+							autocomplete="current-password"
+							name="password"
+							type="password"
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+				<!-- Form Field End -->
+
+				<div>
+					<button
+						type="submit"
+						class="flex w-full justify-center rounded-md bg-purple-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
+						>Einloggen</button
+					>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
---
+++ src/routes/encouragements/+page.svelte
@@ -0,0 +1,67 @@
+<!-- Table View Component Start -->
+<div class="container mx-auto px-4 sm:px-6 lg:px-8 mt-6">
+	<!-- Header Start -->
+	<div class="sm:flex sm:items-center">
+		<div class="sm:flex-auto">
+			<h1 class="text-base font-semibold leading-6 text-gray-900">Encouragements</h1>
+			<p class="mt-2 text-sm text-gray-700">
+				Sei bitte möglichst pervers beim anlegen eines neuen Eintrags
+			</p>
+		</div>
+		<div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+			<button
+				type="button"
+				class="block rounded-md bg-purple-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
+				>Ausloggen</button
+			>
+		</div>
+		<div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+			<button
+				type="button"
+				class="block rounded-md bg-purple-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
+				>Encouragement anlegen</button
+			>
+		</div>
+	</div>
+	<!-- Header End -->
+	<!-- Table Section Start -->
+	<div class="mt-8 flow-root">
+		<div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+			<div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+				<div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+					<table class="min-w-full divide-y divide-gray-300">
+						<thead class="bg-gray-100">
+							<tr>
+								<th
+									scope="col"
+									class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+									>Encouragement</th
+								>
+								<th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6" />
+								<th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6" />
+							</tr>
+						</thead>
+						<tbody class="divide-y divide-gray-200 bg-white">
+							<tr>
+								<td
+									class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"
+									>Ich möchte dir gerne einen blasen</td
+								>
+								<td
+									class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6"
+									><a class="text-purple-600 hover:text-purple-900" href="#">Edit</a></td
+								>
+								<td
+									class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6"
+									><a class="text-purple-600 hover:text-purple-900" href="#">Delete</a></td
+								>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Table Section End -->
+</div>
+<!-- Table View Component End -->
